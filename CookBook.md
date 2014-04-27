f9b49444a3c4716ad6c1331c54b0895f8d3c0d1d
David F. Severski <david@severski.net>
2014-04-27 10:26:50 -0700

    Tidy variable names

--- CookBook.md
+++ CookBook.md
@@ -10,13 +10,21 @@ This is the CodeBook for the MOOC Getting and Cleaning data assignemnt.
 
 Data Columns
 ------------
+Column names are based upon the [Hadley Wickham style guide](http://adv-r.had.co.nz/Style.html).
+
+
+| Stem | Description |
+|------|-------------|
+| subject_id | A numeric [1-30] variable identifying each subject.|
+| activity | The activity being performed in the observation. |
+| tbodyacc | The acceleration signal from the smartphone accelerometer axis in standard gravity units 'g'. |
+| tgravityacct | The body acceleration signal obtained by subtracting the gravity from the total acceleration. |
+| bodgyro | The angular velocity vector measured by the gyroscope for each window sample. The units are radians/second. |
 
 Summary Choices
 ---------------
 The assignment requests that only measurements involving mean or standard deviation are retained. 
-For this work, this is interpreted to involve only those columns with the format of mean() or std().  
-This excludes measurements such as `fbodybodygyrojerkmag-meanfreq()`. These can be included via a 
-simple modification to the grep pattern in `run_analysis.R`.
+For this work, this is interpreted to involve only those columns with the format of mean() or std(). This excludes measurements such as `fbodybodygyrojerkmag-meanfreq()`. These can be included via a simple modification to the grep pattern in `run_analysis.R`.
 
 
 Experimental Study Design
--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,6 @@
 # assume there is a data directory relative to the cwd where the UCI dataset has been extracted
-#setwd('./data/UCI HAR Dataset')
+currentDir <- getwd()
+setwd('./data/UCI HAR Dataset')
 
 # require the plyr library for colwise means
 if (!require(plyr)) {
@@ -7,63 +8,66 @@ if (!require(plyr)) {
 }
 
 # retrieve the names of our features
-featureNames <- read.csv('features.txt', sep='', stringsAsFactors=F, header=F)
+featureNames <- read.csv('features.txt', sep = '', stringsAsFactors = F, header = F)
 
 # clean up the names to tidy standards
 featureNames <- featureNames[,2]
 featureNames <- tolower(featureNames)
 #sub("\\(\\)", "", featureNames[,2])
-#sub*""
+featureNames <- gsub("\\-", "_", featureNames)
+featureNames <- gsub("\\-", "_", featureNames)
+featureNames <- gsub("\\(", "", featureNames)
+featureNames <- gsub("\\)", "", featureNames)
 
 ###
 # Merge the training and the test sets to create one data set.
 ###
 
 # read in the training data
-trainSubjects <- read.csv('./train/subject_train.txt', header=F)
-names(trainSubjects) <- c('subjectID')
+trainSubjects <- read.csv('./train/subject_train.txt', header = F)
+names(trainSubjects) <- c('subject_id')
 
-trainData <- read.csv('train/X_train.txt', sep='', header=F, stringsAsFactors=F)
+trainData <- read.csv('train/X_train.txt', sep = '', header = F, stringsAsFactors = F)
 names(trainData) <- featureNames
 trainData <- cbind(trainSubjects, trainData)
 
 # read in the test data
-testSubjects <- read.csv('./test/subject_test.txt', header=F)
-names(testSubjects) <- c('subjectID')
+testSubjects <- read.csv('./test/subject_test.txt', header = F)
+names(testSubjects) <- c('subject_id')
 
-testData <- read.csv('test/X_test.txt', sep='', header=F, stringsAsFactors=F)
+testData <- read.csv('test/X_test.txt', sep = '', header = F, stringsAsFactors = F)
 names(testData) <- featureNames
 testData <- cbind(testSubjects, testData)
 
 # we only wish to keep the variables that relate to a mean or std. deviation
 # variables are identical in test & train data so varsToKeep can be derived only once
-# we also explictly keep the subjectID
-varsToKeep <- grepl(names(testData), pattern="(-([Mm]ean|std)\\())|subjectID")
+# we also explictly keep the subject ID
+varsToKeep <- grepl(names(testData), pattern = "(_([Mm]ean|std)$)|(_([Mm]ean|std)[^a-zA-Z])|subject_id")
 
-trainData <- subset(trainData, select=varsToKeep)
-testData <- subset(testData, select=varsToKeep)
+trainData <- subset(trainData, select = varsToKeep)
+testData <- subset(testData, select = varsToKeep)
 
 #Uses descriptive activity names to name the activities in the data set
 #Appropriately labels the data set with descriptive activity names. 
 
-activityLabels <- read.csv('activity_labels.txt', sep='', header=F, stringsAsFactors=F)
+activityLabels <- read.csv('activity_labels.txt', sep = '', header = F, stringsAsFactors = F)
 names(activityLabels) <- c('activityID', 'activityName')
 
-activityKey <- read.csv('train/y_train.txt', header=F, sep='', stringsAsFactors=F)
-activityKey <- factor(x=activityKey$V1, labels=activityLabels$activityName)
+activity <- read.csv('train/y_train.txt', header = F, sep = '', stringsAsFactors = F)
+activity <- factor(x = activity$V1, labels = activityLabels$activityName)
 
-trainData <- cbind(trainData, activityKey)
+trainData <- cbind(trainData, activity)
 
-activityKey <- read.csv('test/y_test.txt', header=F, sep='', stringsAsFactors=F)
-activityKey <- factor(x=activityKey$V1, labels=activityLabels$activityName)
+activity <- read.csv('test/y_test.txt', header = F, sep = '', stringsAsFactors = F)
+activity <- factor(x = activity$V1, labels = activityLabels$activityName)
 
-testData <- cbind(testData, activityKey)
+testData <- cbind(testData, activity)
 
 mergedData <- rbind(trainData, testData)
 
 # output the data set
 setwd('../../output')
-write.csv(mergedData, "mergedData.txt", row.names=F)
+write.csv(mergedData, "mergedData.txt", row.names = F)
 
 ###
 #Create a second, independent tidy data set with the average of each variable for each activity and each subject. 
@@ -73,7 +77,10 @@ write.csv(mergedData, "mergedData.txt", row.names=F)
 # summarise_each doesn't install cleanly on Windows as of 4/27/14
 
 #mergedData %.% group_by('subjectID', 'activityKey') %.% summarise_each(funs(mean()))
-summaryStats <- ddply(mergedData, .(subjectID, activityKey), numcolwise(mean))
+summaryStats <- ddply(mergedData, .(subject_id, activity), numcolwise(mean))
 
 # output the data set
-write.csv(summaryStats, "summarisedStats.txt", row.names=F)
+write.csv(summaryStats, "summarisedStats.txt", row.names = F)
+
+#reset cwd to previous value
+setwd(currentDir)
